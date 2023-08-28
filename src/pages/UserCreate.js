f28d67569ec0f04c0418efc2951f17b1ea51ae42
Trung Nguyen <trung.nguyen@kiaisoft.com>
2023-08-28 08:55:34 +0700

    change format of date time picker

--- src/pages/UserCreate.js
+++ src/pages/UserCreate.js
@@ -90,87 +90,6 @@ function UserCreate() {
   }
 
   return (
-    //     <div className="container">
-    //       <div className="row">
-    //         <div className="col-md-12">
-    //           <div className="card">
-    //             <div className="card-header">
-    //               <h4>
-    //                 Add User
-    //                 <Link to="/Users" className="btn btn-danger float-end">
-    //                   Back
-    //                 </Link>
-    //               </h4>
-    //             </div>
-    //             <div className="card-body">
-    //               <form onSubmit={saveUser}>
-    //                 <div className="mb-3">
-    //                   <label htmlFor="name" className="form-label">
-    //                     Name
-    //                   </label>
-    //                   <input
-    //                     type="text"
-    //                     name="name"
-    //                     onChange={handleInput}
-    //                     className="form-control"
-    //                     value={user.name}
-    //                   />
-    //                 </div>
-    //                 <div className="mb-3">
-    //                   <label htmlFor="email" className="form-label">
-    //                     Email
-    //                   </label>
-    //                   <input
-    //                     type="email"
-    //                     name="email"
-    //                     className="form-control"
-    //                     onChange={handleInput}
-    //                     value={user.email}
-    //                   />
-    //                 </div>
-    //                 <div className="mb-3">
-    //                   <label htmlFor="phone" className="form-label">
-    //                     Phone
-    //                   </label>
-    //                   <input
-    //                     type="text"
-    //                     name="phone"
-    //                     className="form-control"
-    //                     onChange={handleInput}
-    //                     value={user.phone}
-    //                   />
-    //                 </div>
-    //                 <div className="mb-3">
-    //                   <label htmlFor="company" className="form-label">
-    //                     Company
-    //                   </label>
-    //                   <input
-    //                     type="text"
-    //                     name="company"
-    //                     className="form-control"
-    //                     onChange={handleInput}
-    //                     value={user.company}
-    //                   />
-    //                 </div>
-    //                 <button type="submit" className="btn btn-primary">
-    //                   Save User
-    //                 </button>
-    //               </form>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //       <div className="dropdown">
-    //   <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    //     Dropdown button
-    //   </button>
-    //   <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-    //     <li><a className="dropdown-item" href="#">Action</a></li>
-    //     <li><a className="dropdown-item" href="#">Another action</a></li>
-    //     <li><a className="dropdown-item" href="#">Something else here</a></li>
-    //   </ul>
-    // </div>
-    //     </div>
     <Stack
       component="form"
       sx={{
@@ -232,7 +151,9 @@ function UserCreate() {
           <DatePicker
             label="Join Date"
             value={joinDate}
-            onChange={(newValue) => setJoinDate(newValue)}
+            onChange={(newValue) => {
+              setJoinDate(newValue.format("YYYY-MM-DD"));
+            }}
           />
         </DemoContainer>
       </LocalizationProvider>
